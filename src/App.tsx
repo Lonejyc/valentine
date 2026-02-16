01c7eef398ed2de23a5ec08e6fc672131a9a46b2
Lonejyc <jocelyn.marcilloux@gmail.com>
2026-02-16 14:38:17 +0100

    chore: refactor button growth

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
 import Button from './components/Button'
 import Letter from './components/Letter'
+import { useProposal } from './hooks/useProposal'
 
 function App() {
-  const [buttonSize, setButtonSize] = useState(1)
-  const [button2Size, setButton2Size] = useState(1)
-  const [isOpen, setIsOpen] = useState(false)
-
-  const handleGrowButton = () => {
-    setButtonSize(buttonSize * 1.6)
-    setButton2Size(button2Size / 1.2)
-  }
+  const { 
+    yesButtonSize, 
+    noButtonSize, 
+    isOpen, 
+    handleAccept, 
+    handleRefusal 
+  } = useProposal()
 
   return (
     <>
@@ -18,8 +17,8 @@ function App() {
       <div className={`absolute inset-0 flex flex-col justify-center items-center gap-8 transition-all duration-700 ease-[linear(0,1.20,0.85,1.10,1)] z-10 ${isOpen ? 'translate-y-[200px] scale-0 opacity-0' : ''}`}>
         <h1 className="text-center text-7xl font-black font-peridot [text-shadow:0_7px_17.1px_rgba(0,0,0,0.25)]">Veux-tu être ma valentine ?</h1>
         <div className="flex justify-center items-center gap-4">
-            <Button name="OUI !" size={buttonSize} onClick={() => setIsOpen(true)}></Button>
-            <Button name="NON." size={button2Size} onClick={handleGrowButton}></Button>
+            <Button name="OUI !" size={yesButtonSize} onClick={handleAccept}></Button>
+            <Button name="NON." size={noButtonSize} onClick={handleRefusal}></Button>
         </div>
       </div>
       <div className="absolute inset-0 flex justify-center items-center">
---
+++ src/hooks/useProposal.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useProposal = () => {
+  const [yesButtonSize, setYesButtonSize] = useState(1)
+  const [noButtonSize, setNoButtonSize] = useState(1)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleRefusal = () => {
+    setYesButtonSize(size => size * 1.6)
+    setNoButtonSize(size => size / 1.2)
+  }
+
+  const handleAccept = () => {
+    setIsOpen(true)
+  }
+
+  return {
+    yesButtonSize,
+    noButtonSize,
+    isOpen,
+    handleRefusal,
+    handleAccept
+  }
+}
